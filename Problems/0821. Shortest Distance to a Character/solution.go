7b14c96d177d1452cad976289af2db0e9d58fa0e
Gary Lu <madcated.lu@gmail.com>
2019-02-26 12:54:40 -0600

    821. Shortest Distance to a Character

---
+++ Problems/0821. Shortest Distance to a Character/solution.go
@@ -0,0 +1,23 @@
+package solution
+
+// Runtime 4ms
+// Memory 6.6 MB
+
+func shortestToChar(S string, C byte) []int {
+	last := -1
+	result := make([]int, len(S))
+	for i := 0; i < len(S); i++ {
+		if S[i] == C {
+			result[i] = 0
+			for j := last + 1; j < i; j++ {
+				if i-j < result[j] || last == -1 {
+					result[j] = i - j
+				}
+			}
+			last = i
+		} else {
+			result[i] = i - last
+		}
+	}
+	return result
+}
