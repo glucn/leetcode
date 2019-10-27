3bf1ccaae6e945ef437f2f724f5f920785dad298
Gary Lu <madcated.lu@gmail.com>
2019-10-26 23:05:35 -0600

    1237. Find Positive Integer Solution for a Given Equation

---
+++ Problems/1237. Find Positive Integer Solution for a Given Equation/solution.java
@@ -0,0 +1,54 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
+// This is the custom function interface.
+// You should not implement it, or speculate about its implementation
+class CustomFunction {
+    // Returns f(x, y) for any given positive integers x and y.
+    // Note that f(x, y) is increasing with respect to both x and y.
+    // i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
+    public int f(int x, int y);
+};
+
+// Runtime 3 ms
+// Memory 35.9 MB
+
+class Solution {
+    public List<List<Integer>> findSolution(CustomFunction customfunction, int z) {
+
+      ArrayList<List<Integer>> result = new ArrayList();
+      boolean[][] visited = new boolean[z*2][z*2];
+
+      Queue<Pair> q = new LinkedList<>();
+      q.add(new Pair(1, 1));
+      visited[1][1] = true;
+
+      while (!q.isEmpty()) {
+        Pair p = q.remove();
+
+        if (customfunction.f(p.i, p.j) == z) {
+          result.add(Arrays.asList(p.i, p.j));
+        }
+        if (customfunction.f(p.i+1, p.j) <= z && !visited[p.i+1][p.j]) {
+          q.add(new Pair(p.i+1, p.j));
+          visited[p.i+1][p.j] = true;
+        }
+        if (customfunction.f(p.i, p.j+1) <= z && !visited[p.i][p.j+1]) {
+          q.add(new Pair(p.i, p.j+1));
+          visited[p.i][p.j+1] = true;
+        }
+      }
+      return result;
+
+    }
+
+  private class Pair {
+    int i;
+    int j;
+
+    Pair(int i, int j) {
+      this.i = i;
+      this.j = j;
+    }
+  }
+}
