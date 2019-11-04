3f2538cf09da95701ba2f744f69e1183141adee4
Gary Lu <madcated.lu@gmail.com>
2019-11-03 22:43:56 -0600

    118. Pascal's Triangle

---
+++ Problems/0118. Pascal's Triangle/solution.go
@@ -0,0 +1,17 @@
+package solution
+
+// Runtime 0 ms, faster than 100%
+// Memory 2.3 MB, less than 100%
+
+func generate(numRows int) [][]int {
+	result := make([][]int, numRows)
+	for i := 0; i < numRows; i++ {
+		result[i] = make([]int, i+1)
+		result[i][0], result[i][i] = 1, 1
+		for j := 1; j < i; j++ {
+			result[i][j] = result[i-1][j-1] + result[i-1][j]
+		}
+	}
+
+	return result
+}
