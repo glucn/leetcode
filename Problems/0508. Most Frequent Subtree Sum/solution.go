367a14239bb3baaeaccd662453320520a935ab66
Gary Lu <madcated.lu@gmail.com>
2019-11-09 16:51:35 -0600

    508. Most Frequent Subtree Sum

---
+++ Problems/0508. Most Frequent Subtree Sum/solution.go
@@ -0,0 +1,44 @@
+package solution
+
+//Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// Runtime 12 ms, faster than 67.19%
+// Memory 7.9 MB, less than 100%
+
+func findFrequentTreeSum(root *TreeNode) []int {
+	count := make(map[int]int)
+
+	dfs(root, count)
+
+	max := 0
+	var result []int
+	for k, v := range count {
+		if v > max {
+			max = v
+			result = []int{k}
+		} else if v == max {
+			result = append(result, k)
+		}
+	}
+	return result
+}
+
+func dfs(node *TreeNode, count map[int]int) int {
+	if node == nil {
+		return 0
+	}
+	if node.Left == nil && node.Right == nil {
+		count[node.Val] = count[node.Val] + 1
+		return node.Val
+	}
+	leftSum := dfs(node.Left, count)
+	rightSum := dfs(node.Right, count)
+	sum := leftSum + rightSum + node.Val
+	count[sum] = count[sum] + 1
+	return sum
+}
