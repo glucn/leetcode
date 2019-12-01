5bb8d93b2cbd598ae446c8c4533ed91b451549b1
Gary Lu <madcated.lu@gmail.com>
2019-12-01 09:15:49 -0600

    1237 - change class to interface

--- Problems/1237. Find Positive Integer Solution for a Given Equation/solution.java
+++ Problems/1237. Find Positive Integer Solution for a Given Equation/solution.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 // This is the custom function interface.
 // You should not implement it, or speculate about its implementation
-class CustomFunction {
+interface CustomFunction {
     // Returns f(x, y) for any given positive integers x and y.
     // Note that f(x, y) is increasing with respect to both x and y.
     // i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
